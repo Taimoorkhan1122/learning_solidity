55366471170bbd2b03b61300e27a0c50d6764f7f
Taimoor khan <khant8k83@gmail.com>
2022-02-04 18:11:07 +0500

    interacting with deployed smart contract

--- step_08/deploy.py
+++ step_08/deploy.py
@@ -50,7 +50,7 @@ abi = json.loads(
 w3 = Web3(Web3.HTTPProvider("HTTP://127.0.0.1:7545"))
 
 chian_id = 1337
-my_address = "0x42ac0dADd895F8669F4D65853CA4533E4a0bdE86"
+my_address = "0xF478A641819D6a960DC2554250351EFA51313a21"
 private_key = os.getenv("PRIVATE_KEY")
 # create contract in python: this will create an instance of Contract object in python
 # Build the contract & deploy Transaction
@@ -64,17 +64,63 @@ nonce = w3.eth.getTransactionCount(my_address)  # NONCE: Number once used
 # 2. Sign Transaction
 # 3. Send Transaction
 
-transaction = SimpleStorage.constructor().buildTransaction(
+initial_transaction = SimpleStorage.constructor().buildTransaction(
     {
         "gasPrice": w3.eth.gas_price,
         "chainId": chian_id,
-        "from": my_address,  
+        "from": my_address,
         "nonce": nonce,
     }
 )
 
-signed_transaction = w3.eth.account.signTransaction(transaction, private_key=private_key)
 
-# send signed transaction 
-tx_hash = w3.eth.send_raw_transaction(signed_transaction.rawTransaction)
-tx_reciept = w3.eth.wait_for_transaction_receipt(tx_hash)
+def createTransaction(transaction):
+    print("signing transcation.... ")
+    # signing transaction (message) with private key
+    signed_tx = w3.eth.account.sign_transaction(
+        transaction, private_key=private_key
+    )
+
+    # sending transaction
+    tx_hash = w3.eth.send_raw_transaction(signed_tx.rawTransaction)
+
+    print("waiting for receipt.... ")
+    tx_reciept = w3.eth.wait_for_transaction_receipt(tx_hash)
+    return tx_reciept
+
+
+# interacting with Contract
+
+# For interacting with deployed contract we need:
+# Contract ABI
+# Contract address
+
+
+# First we will create an instance of our contract class
+simple_storage = w3.eth.contract(
+    address="0x03ad3C8654e6296366127194b016AB353bd0FEDf", abi=abi
+)
+
+# There are two ways of interacting with contract
+# call -> simulating function call with no state change and returning a value
+# transact -> simulating a function call with state change
+print("initial value: ")
+print(simple_storage.functions.retrieve().call())
+
+print("making store call.... ")
+
+simple_storage_transaction = simple_storage.functions.store(25).buildTransaction(
+    {
+        "gasPrice": w3.eth.gas_price,
+        "chainId": chian_id,
+        "from": my_address,
+        "nonce": nonce,
+    }
+)
+
+simple_transation_reciept = createTransaction(simple_storage_transaction)
+
+
+print(
+    f"Transaction Reciept:{simple_transation_reciept} \nUpdated value: {simple_storage.functions.retrieve().call()}"
+)
