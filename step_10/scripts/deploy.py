2ec08bdbabf1f236c254b4450764c5b724b6bd5f
Taimoor khan <khant8k83@gmail.com>
2022-02-10 22:28:51 +0500

    testing smart contract

--- step_10/scripts/deploy.py
+++ step_10/scripts/deploy.py
@@ -1,15 +1,17 @@
-from brownie import accounts, config, SimpleStorage
+from brownie import accounts, config, SimpleStorage, network
 
 
 def deploy_contract():
     # account =  accounts[0]
 
     # account =  accounts.load("my_local_account")
-    account = accounts.add(config["wallets"]["from_key"])
+    account = get_accounts()
     # this single line is equal to all the stuff we did in step 09 to deploy our contract
 
+    print("------- DEPLOYING CONTRACT ------- \n")
     simple_storage = SimpleStorage.deploy({"from": account})
-    print("------- DEPLOYING CONTRACT ------- \n",simple_storage)
+    print(simple_storage)
+
     stored_value = simple_storage.retrieve()
     print("stored Value : ", stored_value)
 
@@ -19,6 +21,11 @@ def deploy_contract():
     print("Updated Value : ", updated_value)
     pass
 
+def get_accounts():
+    if network.show_active() == "development":
+        return accounts[0]
+    else:
+        return accounts.add(config["wallets"]["from_key"])
 
 def main():
     deploy_contract()
