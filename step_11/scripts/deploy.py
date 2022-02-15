a17fd44bc92e3bb8e59f25d68887a8119e8bcd23
Taimoor khan <khant8k83@gmail.com>
2022-02-15 16:47:08 +0500

    deploying to local ganache chain

--- step_11/scripts/deploy.py
+++ step_11/scripts/deploy.py
@@ -1,14 +1,23 @@
-from brownie import FundMe, network, config
-from scripts.helper import get_accounts
+from brownie import FundMe, MockV3Aggregator, network, config
+from scripts.helper import get_accounts, deploy_mock, LOCAL_DEVELOPMENT_ENVIRONMENTS
 
 
 def deploy():
-    if network.show_active() != "development":
+    if network.show_active() not in LOCAL_DEVELOPMENT_ENVIRONMENTS:
         price_feed_address = config["networks"][network.show_active()]["eth_price_feed"]
+    else:
+        deploy_mock()
+        price_feed_address = MockV3Aggregator[-1].address
+
     account = get_accounts()
-    fund_me = FundMe.deploy(price_feed_address, {"from": account}, publish_source=True)
+    fund_me = FundMe.deploy(
+        price_feed_address,
+        {"from": account},
+        publish_source=config["networks"][network.show_active()]["verify"],
+    )
     print(fund_me)
     pass
+
 
 def main():
     deploy()
--- step_11/scripts/helper.py
+++ step_11/scripts/helper.py
@@ -1,7 +1,23 @@
-from brownie import network, accounts,config 
+from brownie import MockV3Aggregator,network, accounts,config 
+from web3 import Web3
+
+DECIMALS = 8
+# This is 2,000
+INITIAL_VALUE = 20000
+LOCAL_DEVELOPMENT_ENVIRONMENTS = ["development", "ganache-local-chain"]
 
 def get_accounts():
-    if network.show_active() == "development":
+    if network.show_active() in LOCAL_DEVELOPMENT_ENVIRONMENTS:
         return accounts[0]
     else:
         return accounts.add(config["wallets"]["from_key"])
+
+def deploy_mock():
+    print(f"The active network is {network.show_active()}")
+    account = get_accounts()
+    if len(MockV3Aggregator) <= 0:
+        print("Deploying Mocks...")
+        MockV3Aggregator.deploy(
+            DECIMALS, Web3.toWei(INITIAL_VALUE, "ether"), {"from": account}
+        )
+        print("Mocks Deployed!")
